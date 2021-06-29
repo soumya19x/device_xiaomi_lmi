6880fb7bcd234184e45750071b7ac33b28144ab7
RealSchnedi <schnedi3@gmail.com>
2021-06-29 23:34:31 +0200

    lmi: Derp bringup

--- derp_lmi.mk
+++ derp_lmi.mk
@@ -8,13 +8,19 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Derp stuff
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
 # Inherit from lmi device
 $(call inherit-product, device/xiaomi/lmi/device.mk)
 
-PRODUCT_NAME := lineage_lmi
+# Derp specific properties
+PRODUCT_PRODUCT_PROPERTIES += \
+  ro.derp.maintainer=Schnedi \
+  ro.derp.cpu=sm8250 \
+  ro.derp.rom=Derp
+
+PRODUCT_NAME := derp_lmi
 PRODUCT_DEVICE := lmi
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
