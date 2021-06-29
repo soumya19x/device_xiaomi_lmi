66ac61c0688e93ccdf602d70e94a5771c5d97bf9
Anton Hansson <antonhansson@gmail.com>
2021-06-30 00:02:47 +0200

    lmi: Remove PRODUCT_AAPT config
    * This config strips unused drawable resources from the device
      to save space, but it is not particularly effective in practice
      (roughly ~30 MB across all partitions)
    
      Remove it to make the system image more generic
    
    Bug: 118676832
    Test: make
    Change-Id: I429dc449866fc0b147cade64c42cf41b00698ffe

--- device.mk
+++ device.mk
@@ -9,10 +9,6 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-derp
 
-# Device uses high-density artwork where available
-PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREF_CONFIG := xhdpi
-
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
