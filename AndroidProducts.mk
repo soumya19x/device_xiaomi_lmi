9aadd233f9ef0dcebe4e888a3cbb229deacfeb77
soumyo19 <fsoummya@gmail.com>
2021-10-07 08:25:23 +0000

    lmi: init dotOS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,12 +1,12 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021 The dotOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_lmi.mk
+    $(LOCAL_DIR)/dot_lmi.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_lmi-userdebug \
-    lineage_lmi-eng
+    dot_lmi-userdebug \
+    dot_lmi-eng
--- dot_lmi.mk
+++ dot_lmi.mk
@@ -12,14 +12,24 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/lmi/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/dot/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_lmi
+PRODUCT_NAME := dot_lmi
 PRODUCT_DEVICE := lmi
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi K30 Pro
 PRODUCT_MANUFACTURER := Xiaomi
+
+# Gapps
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_STOCK_ARCORE := true
+WITH_GAPPS := true
+
+# Inherit some Aosp stuff.
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_BOOT_ANIMATION_RES := 1080
+EXTRA_FOD_ANIMATIONS := true
 
 PRODUCT_CHARACTERISTICS := nosdcard
 
