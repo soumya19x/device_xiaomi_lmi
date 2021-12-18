b32721b69809cb5e7471e78675cbec52755b7f90
RealSchnedi <schnedi3@gmail.com>
2021-12-18 17:56:52 +0100

    Revert "lmi: overlay: FaceUnlock: Define delay for popup camera"

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,7 +28,6 @@ TARGET_KERNEL_CONFIG := vendor/lmi_defconfig
 TARGET_OTA_ASSERT_DEVICE := lmi,lmipro
 
 # Properties
-TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Inherit from the proprietary version
